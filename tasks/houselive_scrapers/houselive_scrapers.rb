d56bb254df247475899de58994c9f403fe029a54
Eric Mill <kprojection@gmail.com>
2010-10-18 16:01:03 -0400

    Added a bridge for the video scraperrs

---
+++ tasks/houselive_scrapers/houselive_scrapers.rb
@@ -0,0 +1,8 @@
+class HouseliveScrapers
+  
+  def self.run(options = {})
+    script = File.join File.dirname(__FILE__), "grab_videos.py"
+    system "python #{script} #{options[:config][:mongoid]['database']}"
+  end
+  
+end
