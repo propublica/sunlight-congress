c647e149147ab4580cb898746fca59757831c5e0
Eric Mill <kprojection@gmail.com>
2010-10-19 11:44:57 -0400

    Fixed up environment to run from any working directory, and added back my unit tests for bill parsing

--- config/environment.rb
+++ config/environment.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 require 'mongoid'
 
 def config
-  @config ||= YAML.load_file 'config/config.yml'
+  @config ||= YAML.load_file File.join(File.dirname(__FILE__), "config.yml")
 end
 
 configure do
   Mongoid.configure {|c| c.from_hash config[:mongoid]}
 end
 
-Dir.glob('models/*.rb') {|filename| load filename}
+Dir.glob(File.join(File.dirname(__FILE__), "../models/*.rb")) {|filename| load filename}
