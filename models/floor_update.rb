f4d3ae389feb3143c03b9132e4eeb0447157db51
Eric Mill <kprojection@gmail.com>
2010-11-18 16:27:12 -0500

    Moved code

--- models/floor_update.rb
+++ models/floor_update.rb
@@ -4,12 +4,12 @@ class FloorUpdate
   field :chamber
   field :legislative_day
   
-  def self.default_order
-    :timestamp
-  end
-  
   def self.singular_api?
     false
+  end
+  
+  def self.default_order
+    :timestamp
   end
   
   def self.basic_fields
