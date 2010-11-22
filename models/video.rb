cc63f7574e4c5900f2d3d5ca3c7c360a95d9dca1
Eric Mill <kprojection@gmail.com>
2010-11-22 16:55:17 -0500

    Overrode timestamp_id on Video to say it's a string

--- models/video.rb
+++ models/video.rb
@@ -3,6 +3,9 @@ class Video
   
   index :timestamp_id, :unique => true
   
+  # timestamp_id is stored as a String
+  field :timestamp_id, :type => String
+  
   def self.unique_keys
     [:timestamp_id]
   end
