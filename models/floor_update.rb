5472977d9a407ac7d16033d164c15b8ad89f5d29
Eric Mill <kprojection@gmail.com>
2011-01-19 15:55:03 -0500

    Indexing roll and bill IDs, and including in basic fields, on floor updates

--- models/floor_update.rb
+++ models/floor_update.rb
@@ -3,13 +3,15 @@ class FloorUpdate
   
   index :chamber
   index :legislative_day
+  index :roll_ids
+  index :bill_ids
   
   def self.default_order
     :timestamp
   end
   
   def self.basic_fields
-    [:chamber, :legislative_day, :timestamp, :events]
+    [:chamber, :legislative_day, :timestamp, :events, :roll_ids, :bill_ids]
   end
   
   def self.search_fields
