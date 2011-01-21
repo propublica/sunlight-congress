e167ebab13a37a7f04d046514500e1a4cebd63e3
Eric Mill <kprojection@gmail.com>
2011-01-21 13:13:43 -0500

    Incorporate legislator_ids into basic section, index them

--- models/floor_update.rb
+++ models/floor_update.rb
@@ -5,13 +5,14 @@ class FloorUpdate
   index :legislative_day
   index :roll_ids
   index :bill_ids
+  index :legislator_ids
   
   def self.default_order
     :timestamp
   end
   
   def self.basic_fields
-    [:chamber, :legislative_day, :timestamp, :events, :roll_ids, :bill_ids]
+    [:chamber, :legislative_day, :timestamp, :events, :roll_ids, :bill_ids, :legislator_ids]
   end
   
   def self.search_fields
