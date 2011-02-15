7b73a6da4c6d804057a81918c228dbce0b0e2345
Eric Mill <kprojection@gmail.com>
2011-02-15 16:42:52 -0500

    Stripping out weird HTML and handling senators with states

--- tasks/floor_updates_live_senate/floor_updates_live_senate.py
+++ tasks/floor_updates_live_senate/floor_updates_live_senate.py
@@ -25,7 +25,7 @@ def run(db, options = {}):
     headers = soup.findAll('h3')
     
     for header in headers:
-        fallback_time = section_time(header)
+        fallback_time = section_time(header, day)
         
         section = header.parent.findAll('tr')[-1]
         items = section.findChild("td").findChildren("p")
@@ -39,6 +39,7 @@ def run(db, options = {}):
         current_events = []
         current_name = None
         current_time = None
+        current_bioguide_id = None
         
         # go over each p tag, find the substantive ones, record it as an event
         for item in items:
@@ -47,17 +48,26 @@ def run(db, options = {}):
           str = re.sub("^o?SUMMARY", "", str) # not sure why the o and the SUMMARY appear
           
           if str:
+            # extraneous weird xml html check, this occurs sometimes
+            matches = re.search("</font>([^<]+)$", str)
+            if matches:
+              str = matches.groups()[0]
+            
             # first, check if it's a name and time line
-            new_name, new_time = name_and_time(str, day)
+            new_name, new_state, new_time, new_bioguide_id = name_and_time(db, str, day)
             if new_name and new_time:
               # if so, save any events we've been accruing, if there are any
               if current_events:
-                save_update(db, current_time, legislative_day, current_events)
+                save_update(db, current_time, legislative_day, current_events, current_bioguide_id)
               
               # then clear the accruing events and start a new one with the new name and timestamp
               current_events = []
-              current_name = new_name
+              if new_state:
+                current_name = "Sen. %s (%s)" % (new_name, new_state)
+              else:
+                current_name = new_name
               current_time = new_time
+              current_bioguide_id = new_bioguide_id
               continue
             
             # otherwise, if there's content, add it to the accruing events for this person and this time
@@ -69,7 +79,7 @@ def run(db, options = {}):
         
         # if there were any left when we're all done, save them
         if current_events:
-          save_update(db, current_time, legislative_day, current_events)
+          save_update(db, current_time, legislative_day, current_events, current_bioguide_id)
         
         
         count += 1
@@ -78,23 +88,37 @@ def run(db, options = {}):
     db.success("Updated or created %s floor updates for the Senate" % count)
 
 
-def save_update(db, timestamp, legislative_day, events):
+def save_update(db, timestamp, legislative_day, events, bioguide_id):
   event = db.get_or_initialize("floor_updates", {
     'timestamp': timestamp, 
     'chamber': 'senate'
   })
+  
+  event['bioguide_ids'] = [bioguide_id]
   
   event['events'] = [events]
   event['legislative_day'] = legislative_day
   
   db['floor_updates'].save(event)
 
-# example: "&nbsp;Senator Durbin:(2:20 PM)"
-def name_and_time(text, day):
-  matches = re.search("(Senator [^:]+):\((\d+:\d+ (?:AM|PM))", text)
+# example: "Senator Durbin:(2:20 PM)"
+# example: "Senator Udall-CO:(2:20 PM)"
+def name_and_time(db, text, day):
+  matches = re.search("^Senator ([^:]+):\((\d+:\d+ (?:AM|PM))", text)
   if matches:
     name = matches.groups()[0]
     time_str = matches.groups()[1]
+  
+    # if name has a state suffix, split it now
+    bioguide_id = None
+    state = None
+    matches = re.search("(-([A-Z]{2}))$", name)
+    if matches:
+      name = name.replace(matches.groups()[0], '')
+      state = matches.groups()[1]
+    
+    # match the name regardless of whether we have a state
+    bioguide_id = bioguide_id_for(db, name, state)
   
     # timestamp
     time_of_day = time.strptime(time_str, "%I:%M %p")
@@ -103,11 +127,22 @@ def name_and_time(text, day):
     # entries were duplicates of each other.
     timestamp = datetime.datetime(day.year, day.month, day.day, time_of_day.tm_hour, time_of_day.tm_min)
   
-    return (name, timestamp)
+    return (name, state, timestamp, bioguide_id)
     
   else:
-    return (None, None)
+    return (None, None, None, None)
 
+# the state should always make the name unambiguous (I hope)
+def bioguide_id_for(db, name, state = None):
+  query = {'last_name': name}
+  if state:
+    query['state'] = state
+  
+  results = db['legislators'].find(query)
+  if results.count() > 0:
+    return results[0]['bioguide_id']
+  else:
+    return None
 
 # return timestamp from a whole section of updates
 def section_time(header, day):
