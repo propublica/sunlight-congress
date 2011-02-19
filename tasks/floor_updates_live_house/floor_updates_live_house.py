6948dae59bdcc38efe98cfb5521525bda5784ecc
Eric Mill <kprojection@gmail.com>
2011-02-19 09:42:48 -0500

    Fixed issue where the house floor update parser wasn't handling post-midnight work correctly

--- tasks/floor_updates_live_house/floor_updates_live_house.py
+++ tasks/floor_updates_live_house/floor_updates_live_house.py
@@ -27,29 +27,46 @@ def run(db, options = {}):
         
     date_field = date_objs[0].strip()
     
-    day_of_pieces = time.strptime(date_field.replace('LEGISLATIVE DAY OF ', ''), "%B %d, %Y")
-    day_of_string = time.strftime("%m/%d/%Y", day_of_pieces)
-    
-    year = day_of_pieces[0]
+    # legislative_day stays constant through all events
+    legislative_day = datetime.datetime.strptime(date_field.replace('LEGISLATIVE DAY OF ', ''), "%B %d, %Y")
+    year = legislative_day.year
     session = rtc_utils.current_session(year)
     
-    count = 0
+    # as timestamps tick up and potentially go past midnight to the next day,
+    # (and even potentially multiple days, though that is rare)
+    # use marks in the sand to figure out what day to advance the timestamp to,
+    # even as the "legislative_day" remains the same.
+    current_day = legislative_day
+    # hold on to the last timestamp - if the next one is ever earlier than the last one,
+    # we know it's time to advance the current day
+    last_timestamp = None
     
+    count = 0
     rows = soup.findAll('dt')
+    
+    rows.reverse() # go in chronological order, so that we can track how the days advance
+    
     for row in rows:
         if row.b.contents:
             time_string = row.b.contents[0]
             t = re.compile("(\d+:\d{2}\s+(A|P)\.M\.)")
-            m = t.findall(time_string)
+            matches = t.findall(time_string)
             
-            if m:
+            if matches:
+                time_string = rtc_utils.remove_extra_spaces(matches[0][0].replace('.', ''))
                 
-                time_string = rtc_utils.remove_extra_spaces(m[0][0].replace('.', ''))
-                time_string = "%s %s" % (day_of_string, time_string)
+                # make a test timestamp with this time and the current day
+                event_time = datetime.datetime.strptime(time_string, "%I:%M %p")
+                event_time = datetime.datetime(current_day.year, current_day.month, current_day.day, event_time.hour, event_time.minute, tzinfo=rtc_utils.EST())
                 
-                event_time_components = time.strptime(time_string, "%m/%d/%Y %I:%M %p")
-                event_time = datetime.datetime(*event_time_components[0:5], tzinfo=rtc_utils.EST())
-                legislative_day = time.strftime("%Y-%m-%d", event_time_components)
+                # if the time has ticked over to the next day, advance the current day,
+                # and advance the event time accordingly
+                if last_timestamp and (event_time < last_timestamp):
+                    current_day = current_day.replace(current_day.year, current_day.month, current_day.day + 1)
+                    event_time = event_time.replace(current_day.year, current_day.month, current_day.day)
+                
+                # now that we've settled the real time, store it as the last timestamp
+                last_timestamp = event_time
                 
                 events = []
                 maybe_events = row.findNextSibling().findAll(text=True)
@@ -61,15 +78,16 @@ def run(db, options = {}):
                 # put in chronological order
                 events.reverse()
                 
-                all_events = "\n".join(events)
-                
                 event = db.get_or_initialize("floor_updates", {
                   'timestamp': event_time,
                   'chamber': 'house'
                 })
                 
                 event['events'] = events
-                event['legislative_day'] = legislative_day
+                event['legislative_day'] = legislative_day.strftime("%Y-%m-%d")
+                
+                # extract entities
+                all_events = "\n".join(events)
                 event['bill_ids'] = rtc_utils.extract_bills(all_events, session)
                 event['roll_ids'] = rtc_utils.extract_rolls(all_events, 'house', year)
                 event['legislator_ids'] = rtc_utils.extract_legislators(all_events, 'house', db)[1]
