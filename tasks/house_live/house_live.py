c94c9e9e23fa0fe7dfd9c834ce0dd262926a2af9
kaitlin <kaitlin@kaitlin-work.(none)>
2011-03-24 14:17:14 -0400

    added hls urls to house videos

--- tasks/house_live/house_live.py
+++ tasks/house_live/house_live.py
@@ -4,6 +4,7 @@ import urllib2
 import datetime
 from dateutil.tz import *
 import time
+import re
 
 
 rss_url = "http://houselive.gov/VPodcast.php?view_id=2"
@@ -29,12 +30,21 @@ def run(db, options = {}):
         video_obj['title'] = video.title
         video_obj['description'] = "HouseLive.gov feed for " + timestamp_obj.strftime("%B %d, %Y")
         link = video.link
+        guid_matches = re.findall("clerkhouse_([\-|\w|\d]*)\.mp4", url)
+        if guid_matches:
+            guid = guid_matches[0]
+        else:
+            guid = None
         clip_id = re.search("(clip_id=)(\d+)", link).groups()[1]
         video_obj['clip_id'] = clip_id
         if video_obj.has_key('clip_urls'):
             video_obj['clip_urls']['mp4'] = url
+            if guid:
+                video_obj['clip_urls']['hls'] = "http://207.7.154.46:1935/OnDemand/_definst_/mp4:clerkhouse/clerkhouse_%s.mp4/playlist.m3u8" % guid
         else:
             video_obj['clip_urls'] = {'mp4' : url }
+            if guid:
+                video_obj['clip_urls']['hls'] = "http://207.7.154.46:1935/OnDemand/_definst_/mp4:clerkhouse/clerkhouse_%s.mp4/playlist.m3u8" % guid
 
         video_obj['created_at'] = add_date
         video_obj['chamber'] = 'house'
