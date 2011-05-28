fe40781d31f66ab011fe14a9911b829069e454bd
Eric Mill <kprojection@gmail.com>
2011-05-28 18:01:37 -0400

    Minorly enhanced shell task, in the course of working with unfinished real code

--- tasks/bills_fulltext_archive/bills_fulltext_archive.rb
+++ tasks/bills_fulltext_archive/bills_fulltext_archive.rb
@@ -6,30 +6,61 @@ class BillsFulltextArchive
     bill_count = 0
     version_count = 0
     
+    
+    puts "Rsyncing to GovTrack for bill text..." if options[:debug]
     FileUtils.mkdir_p "data/govtrack/#{session}/bill_text"
     unless system("rsync -az govtrack.us::govtrackdata/us/bills.text/#{session}/ data/govtrack/#{session}/bill_text/")
       Report.failure self, "Couldn't rsync to Govtrack.us for bill text."
       return
     end
     
-    bills = Bill.where(:session => session).all
     
-    bills.each do |bill|
+#     client = elastic_search_for 'bills', 'bill_text'
+    
+    
+    fields = Bill.basic_fields + [:summary]
+    bills = Bill.where(:session => session, :abbreviated => false).only(fields)
+    
+      
+    # debug
+    bills = bills.limit(5)
+    
+    bills.all.each do |bill|
       type = Utils.govtrack_type_for bill.bill_type
       
       # find all the versions of text for that bill, load them in
-      versions = Dir.glob("data/govtrack/#{session}/bill_text/#{type}/#{type}#{bill.number}[a-z]*.txt")
-      versions.each do |version|
-        code = File.basename version
+      version_files = Dir.glob("data/govtrack/#{session}/bill_text/#{type}/#{type}#{bill.number}[a-z]*.txt")
+      version_files.each do |file|
+        code = File.basename file
         code["#{type}#{bill.number}"] = ""
         code[".txt"] = ""
         
-        puts "[#{bill.bill_id}] version #{code}"
+#         full_text = File.read file
+#         
+#         document = {
+#           :version_code => code,
+#           :full_text => full_text
+#         }
+#         
+#         fields.each do |field|
+#           document.merge! field => bill.attributes[field.to_s]
+#         end
+#         
+#         client.index(
+#           document,
+#           :id => "#{bill.bill_id}-#{code}"
+#         )
+#         
+#         puts "[#{bill.bill_id}][#{code}] Indexed." if options[:debug]
+        
         version_count += 1
       end
       
       bill_count += 1
     end
+    
+#     # make sure queries are ready
+#     client.refresh
     
     Report.success self, "Loaded in full text of #{bill_count} bills (#{version_count} versions) for session ##{session} from GovTrack.us."
   end
