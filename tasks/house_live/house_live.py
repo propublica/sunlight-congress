adad91a79af576ec48f2d7f21dfd5fbee82d787c
kaitlin <kaitlin@kaitlin-work.(none)>
2011-07-20 16:20:33 -0400

    updated granicus ip address

--- tasks/house_live/house_live.py
+++ tasks/house_live/house_live.py
@@ -40,11 +40,11 @@ def run(db, options = {}):
         if video_obj.has_key('clip_urls'):
             video_obj['clip_urls']['mp4'] = url
             if guid:
-                video_obj['clip_urls']['hls'] = "http://207.7.154.46:1935/OnDemand/_definst_/mp4:clerkhouse/clerkhouse_%s.mp4/playlist.m3u8" % guid
+                video_obj['clip_urls']['hls'] = "http://207.7.154.31:1935/OnDemand/_definst_/mp4:clerkhouse/clerkhouse_%s.mp4/playlist.m3u8" % guid
         else:
             video_obj['clip_urls'] = {'mp4' : url }
             if guid:
-                video_obj['clip_urls']['hls'] = "http://207.7.154.46:1935/OnDemand/_definst_/mp4:clerkhouse/clerkhouse_%s.mp4/playlist.m3u8" % guid
+                video_obj['clip_urls']['hls'] = "http://207.7.154.31:1935/OnDemand/_definst_/mp4:clerkhouse/clerkhouse_%s.mp4/playlist.m3u8" % guid
 
         video_obj['created_at'] = add_date
         video_obj['chamber'] = 'house'
