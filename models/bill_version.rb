46f6e45a58c0f9edf42be91f5bfb537b8c1b879f
Eric Mill <kprojection@gmail.com>
2011-09-01 14:45:31 -0400

    adding a bill summary field as searchable on bill_version

--- models/bill_version.rb
+++ models/bill_version.rb
@@ -2,5 +2,5 @@ class BillVersion
   include Searchable::Model
   
   result_fields :version_code, :bill, :bill_version_id
-  searchable_fields :full_text
+  searchable_fields :full_text, "bill.summary"
 end
