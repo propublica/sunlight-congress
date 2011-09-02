0f432e5b69911fdb11857c260e36759229669115
Eric Mill <kprojection@gmail.com>
2011-09-02 16:20:10 -0400

    Added more searchable fields on bill version

--- models/bill_version.rb
+++ models/bill_version.rb
@@ -2,5 +2,5 @@ class BillVersion
   include Searchable::Model
   
   result_fields :version_code, :bill, :bill_version_id
-  searchable_fields :full_text, "bill.summary"
+  searchable_fields :full_text, "bill.summary", "bill.keywords", "bill.official_title", "bill.popular_title", "bill.short_title"
 end
