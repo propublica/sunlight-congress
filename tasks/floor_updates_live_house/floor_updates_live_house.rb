18c88310b4d5ccec1dc213631763321c3054bffa
Eric Mill <kprojection@gmail.com>
2011-09-26 14:18:05 -0400

    New House floor updates parser, everything done but entity extraction, safe to re-instate

--- tasks/floor_updates_live_house/floor_updates_live_house.rb
+++ tasks/floor_updates_live_house/floor_updates_live_house.rb
@@ -4,13 +4,65 @@ require 'nokogiri'
 class FloorUpdatesLiveHouse
   
   def self.run(options = {})
-    count = 0
-    failures = []
-    
     url = get_xml_url options
     return unless url # a warning report will have been filed
     
-    puts url
+    count = 0
+    failures = []
+    
+    xml = nil
+    begin
+      xml = open url
+      xml = xml.read
+    rescue Timeout::Error, Errno::ECONNRESET, Errno::ETIMEDOUT, Errno::ENETUNREACH
+      Report.warning self, "Network error on fetching the floor log, can't go on."
+      return
+    end
+    
+    unless xml =~ /^<\?xml/
+      Report.warning self, "No XML for that day, can't go on."
+      return
+    end
+    
+    doc = Nokogiri::XML xml
+    
+    
+    session = session_for doc
+    chamber = 'house'
+    legislative_day = legislative_day_for doc
+    legislative_day_stamp = legislative_day.strftime "%Y-%m-%d"
+    year = legislative_day.year
+    
+    (doc/:floor_action).each do |action|
+      timestamp = Time.parse action.at("action_time")['for-search']
+      item = action.at("action_description").inner_text.strip
+      
+      unless update = FloorUpdate.where(:timestamp => timestamp, :chamber => chamber).first
+        update = FloorUpdate.new(:timestamp => timestamp, :chamber => chamber, :events => item)
+      end
+      
+      update.attributes = {
+        :session => session,
+        :legislative_day => legislative_day_stamp,
+        :bill_ids => bill_ids_for(item, session),
+        :roll_ids => roll_ids_for(item, year),
+        :legislator_ids => legislator_ids_for(item)
+      }
+      
+      if update.save
+        count += 1
+      else
+        failures << floor_update.attributes
+        puts "Failed to save floor update, will file report"
+      end
+      
+    end
+    
+    if failures.any?
+      Report.failure self, "Had #{failures.size} failures, attributes attached", :failures => failures
+    end
+    
+    Report.success self, "Updated/created #{count} floor updates in the House."
   end
   
   def self.get_xml_url(options)
@@ -45,5 +97,92 @@ class FloorUpdatesLiveHouse
       end
     end
   end
+  
+  def self.session_for(doc)
+    doc.at("legislative_congress")['congress'].to_i
+  end
+  
+  def self.legislative_day_for(doc)
+    Time.parse doc.at("legislative_day").text
+  end
+  
+  def self.roll_ids_for(text, year)
+    []
+  end
+  
+  def self.bill_ids_for(text, session)
+    []
+  end
+  
+  def self.legislator_ids_for(text)
+    []
+  end
+  
+#   def extract_rolls(data, chamber, year):
+#     roll_ids = []
+#     
+#     roll_re = re.compile('Roll (?:no.|Call) (\d+)', flags=re.IGNORECASE)
+#     roll_matches = roll_re.findall(data)
+#     
+#     if roll_matches:
+#       for number in roll_matches:
+#           roll_id = "%s%s-%s" % (chamber[0], number, year)
+#           if roll_id not in roll_ids:
+#               roll_ids.append(roll_id)
+#     
+#     return roll_ids
+#     
+# def extract_bills(text, session):
+#     bill_ids = []
+#     
+#     p = re.compile('((S\.|H\.)(\s?J\.|\s?R\.|\s?Con\.| ?)(\s?Res\.)*\s?\d+)', flags=re.IGNORECASE)
+#     bill_matches = p.findall(text)
+#     
+#     if bill_matches:
+#         for b in bill_matches:
+#             bill_text = "%s-%s" % (b[0].lower().replace(" ", '').replace('.', '').replace("con", "c"), session)
+#             if bill_text not in bill_ids:
+#                 bill_ids.append(bill_text)
+#     
+#     return bill_ids
+# 
+# def extract_legislators(text, chamber, db):
+#     legislator_names = []
+#     bioguide_ids = []
+#     
+#     possibles = []
+#     
+#     if chamber == "house":
+#         name_re = re.compile('((M(rs|s|r)\.){1}\s((\s?[A-Z]{1}[A-Za-z-]+){0,2})(,\s?([A-Z]{1}[A-Za-z-]+))?((\sof\s([A-Z]{2}))|(\s?\(([A-Z]{2})\)))?)')
+#       
+#         name_matches = re.findall(name_re, text)
+#         if name_matches:
+#             for n in name_matches:
+#                 raw_name = n[0]
+#                 query = {"chamber": "house"}
+#                 
+#                 if n[1]:
+#                     if n[1] == "Mr." : query["gender"] = 'M'
+#                     else: query['gender'] = 'F'
+#                 if n[3]:
+#                     query["last_name"] = n[3]
+#                 if n[6]:
+#                     query["first_name"] = n[6]
+#                 if n[9]:
+#                     query["state"] = n[9]
+#                 elif n[11]:
+#                     query["state"] = n[11]
+#                     
+#                 possibles = db['legislators'].find(query)
+#             
+#             if possibles.count() > 0:
+#                 if text not in legislator_names:
+#                     legislator_names.append(raw_name)
+#                     
+#             for p in possibles:
+#                 if p['bioguide_id'] not in bioguide_ids:
+#                     bioguide_ids.append(p['bioguide_id'])
+#     
+#     return (legislator_names, bioguide_ids)
   
 end
