35cc518bdfaa3ec9b63c9ac4c5a7e3017b7af697
Eric Mill <kprojection@gmail.com>
2011-12-06 16:22:37 -0500

    Integrated video clips into RTC and made captions searchable - highlighting is supported with term vector highlighting

--- models/clip.rb
+++ models/clip.rb
@@ -2,7 +2,7 @@ class Clip
 
   include Searchable::Model
   
-  result_fields :video_id, :id, :video_clip_id, :offset, :duration, :events
-  searchable_fields :events
+  result_fields :video_id, :id, :video_clip_id, :offset, :duration, :events, :srt_link, :legislator_names, :rolls
+  searchable_fields :events, "captions.text"
 
 end
