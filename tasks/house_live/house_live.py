faa6ef07db0a2881378dc2c9f48c796e9cc46522
kaitlin <kaitlin@kaitlin-work.(none)>
2011-12-19 12:35:24 -0500

    changed a single subobject on the captions to a single string

--- tasks/house_live/house_live.py
+++ tasks/house_live/house_live.py
@@ -255,7 +255,7 @@ def get_clip_captions(video, clip):
     for cap in captions:
         cap_str += cap['text']
         
-    return [{'text':cap_str},]
+    return cap_str
      
 
 def query_api(db, api_url, data=None):
