6494fdeb45b14290aa9d08220c89dd30b8da32c1
Eric Mill <kprojection@gmail.com>
2011-12-20 16:39:43 -0500

    Adjusted searchable fields for simpler clips structure

--- models/clip.rb
+++ models/clip.rb
@@ -3,6 +3,6 @@ class Clip
   include Searchable::Model
   
   result_fields :video_id, :id, :video_clip_id, :offset, :duration, :events, :srt_link, :legislator_names, :rolls, :bills, :bioguide_ids
-  searchable_fields :events, "captions.text", :legislator_names
+  searchable_fields :events, :captions, :legislator_names
 
 end
