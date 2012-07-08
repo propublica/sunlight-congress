6a797cf8c174fad87cf92d7810d360fa7599df12
Eric Mill <konklone@gmail.com>
2012-07-08 16:57:49 -0400

    Removed unused code from committee hearings script

--- tasks/committee_hearings_senate/committee_hearings_senate.py
+++ tasks/committee_hearings_senate/committee_hearings_senate.py
@@ -7,11 +7,6 @@ import HTMLParser
 
 
 def run(db, es, options = {}):
-    senate_count = senate_hearings(db)
-    db.success("Updated or created %s Senate committee hearings" % senate_count)
-
-
-def senate_hearings(db):
     try:
       page = urllib2.urlopen("http://www.senate.gov/general/committee_schedules/hearings.xml")
     except:
@@ -85,79 +80,7 @@ def senate_hearings(db):
           
           count += 1
       
-      return count
-
-def house_hearings(db):
-    today = datetime.date.today()
-    session = rtc_utils.current_session(today.year)
-    
-    try:
-      url = "http://www.govtrack.us/data/us/%s/committeeschedule.xml" % session
-      page = urllib2.urlopen(url).read()
-    except:
-      # not expected for GovTrack's server to be flakey, thus warning and not note
-      db.warning("Couldn't load GovTrack house hearings feed, can't proceed")
-      
-    else:
-      soup = BeautifulStoneSoup(page)
-      
-      count = 0
-      
-      problems = []
-      
-      meetings = soup.findAll('meeting')
-      for meeting in meetings:
-          if meeting['where'] != 'h':
-              continue
-          
-          committee_id = rtc_utils.committee_id_for(meeting['committee-id'], meeting['committee'])
-          if not committee_id:
-              problems.append("Blank committee ID for %s" % meeting['committee'])
-              continue
-              
-          committee = committee_for(db, committee_id)
-          if not committee:
-              problems.append("Couldn't locate committee by committee_id (%s) while parsing House committee hearings" % committee_id)
-          
-          
-          bill_ids = []
-          bills = meeting.findAll('bill')
-          for bill in bills:
-              bill_type = rtc_utils.bill_type_for(bill['type'])
-              bill_ids.append("%s%s-%s" % (bill_type, bill['number'], bill['session']))
-          
-          
-          occurs_at = rtc_utils.parse_iso8601(meeting['datetime'])
-          
-          legislative_day = rtc_utils.in_est(occurs_at).strftime("%Y-%m-%d")
-          time_of_day = rtc_utils.in_est(occurs_at).strftime("%I:%M %p")
-          description = meeting.find('subject').text
-          
-          hearing = db.get_or_initialize('committee_hearings', {
-              'chamber': 'house', 
-              'committee_id': committee_id, 
-              'occurs_at': occurs_at
-          })
-          
-          hearing.update({
-              'description': description, 
-              'legislative_day': legislative_day,
-              'time_of_day': time_of_day,
-              'session': session,
-              'bill_ids': bill_ids
-          })
-          
-          if committee:
-              hearing['committee'] = committee
-              
-          db['committee_hearings'].save(hearing)
-          
-          count += 1
-      
-      if len(problems) > 0:
-        db.warning("%i problems while going through House committee hearings" % len(problems), {'messages': problems})
-        
-      return count
+      db.success("Updated or created %s Senate committee hearings" % count)
 
 def committee_for(db, committee_id):
   committee = db['committees'].find_one({'committee_id': committee_id}, fields=["committee_id", "name", "chamber"])
