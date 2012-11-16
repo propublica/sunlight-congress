40204337fd2b0f21dc50c2ec5cc664d9c1debeb9
Eric Mill <konklone@gmail.com>
2012-11-16 16:46:50 -0500

    Converting committee script to use us data for basic attributes

--- tasks/committees/committees.rb
+++ tasks/committees/committees.rb
@@ -1,45 +1,69 @@
 require 'sunlight'
 
 class Committees
+
+  # options:
+  #   cache: don't redownload the YAML files
   
   def self.run(options = {})
-    Sunlight::Base.api_key = options[:config]['sunlight_api_key']
+
+    # wipe and re-clone the unitedstates legislators repo
+    unless options[:cache]
+      FileUtils.mkdir_p "data/unitedstates"
+      FileUtils.rm_rf "data/unitedstates/congress-legislators"
+      unless system "git clone git://github.com/unitedstates/congress-legislators.git data/unitedstates/congress-legislators"
+        Report.error self, "Couldn't clone legislator data from unitedstates."
+        return false
+      end
+      puts
+    end
+
+    
+    puts "Loading in YAML files..." if options[:debug]
+    current_committees = YAML.load open("data/unitedstates/congress-legislators/committees-current.yaml")
+    memberships = YAML.load open("data/unitedstates/congress-legislators/committee-membership-current.yaml")
     
     bad_committees = []
     count = 0
+    sub_count = 0
     
-    senate = Sunlight::Committee.all_for_chamber 'Senate'
-    house = Sunlight::Committee.all_for_chamber 'House'
-    joint = Sunlight::Committee.all_for_chamber 'Joint'
-    
-    (senate + house + joint).each do |api_committee|
-      committee = Committee.find_or_initialize_by committee_id: api_committee.id
+    # store committees and subcommittees as peers, *and* nest subcommittees inside their committee
+    current_committees.each do |us_committee|
+      committee_id = us_committee['thomas_id']
+      committee = Committee.find_or_initialize_by committee_id: committee_id
       
-      committee.attributes = attributes_for api_committee
+      committee.attributes = attributes_for us_committee
+      # committee.attributes = memberships_for us_committee, memberships
+
+      # us_committees['subcommittees'].each do |subcommittee|
+      # end
       
-      unless committee.save
-        bad_committees << {:attributes => committee.attributes, :error_messages => committee.errors.full_messages}
-      end
-      
+      committee.save!
       count += 1
     end
     
     if bad_committees.any?
-      Report.warning self, "Failed to save #{bad_committees.size} committee, last bad one attached", :bad_committee => bad_committees.first
+      Report.warning self, "Failed to save #{bad_committees.size} committee, attached", bad_committee: bad_committees
     end
     
-    Report.success self, "Processed #{count} committees from API - total count in database now: #{Committee.count}"
+    Report.success self, "Processed #{count} current committees"
   end
   
-  def self.attributes_for(api_committee)
+  def self.attributes_for(us_committee, parent_id = nil)
     attributes = {
-      name: api_committee.name,
-      chamber: api_committee.chamber.downcase,
-      committee_id: api_committee.id
+      name: us_committee['name'],
+      chamber: us_committee['type']
     }
 
-    if api_committee.subcommittees
-      attributes[:subcommittees] = api_committee.subcommittees.map {|sc| attributes_for sc}
+    # optional fields
+    ['address', 'senate_committee_id', 'house_committee_id', 'url', 'phone'].each do |field|
+      if us_committee.has_key?(field)
+        attributes[field.to_sym] = us_committee[field]
+      end
+    end
+
+    if (us_committee['type'] == 'house') and us_committee['address']
+      attributes[:office] = us_committee['address'].split("; ").first
     end
     
     attributes
