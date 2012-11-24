ce5c13a8177b107b5d7120dd1711bb8c8333eecf
Eric Mill <konklone@gmail.com>
2012-11-24 15:29:04 -0500

    Don't bother looking for membership of old committees, keep records of missing congress records, and whether members are missing for current committees

--- tasks/committees/committees.rb
+++ tasks/committees/committees.rb
@@ -4,8 +4,11 @@ class Committees
 
   # options:
   #   cache: don't redownload the YAML files
+  #   session: session of Congress, for purpose of caring about
+  #     whether we have membership data or not
   
   def self.run(options = {})
+    session = options[:session] ? options[:session].to_i : Utils.current_session
 
     # wipe and re-clone the unitedstates legislators repo
     unless options[:cache]
@@ -23,10 +26,16 @@ class Committees
     current_committees = YAML.load open("data/unitedstates/congress-legislators/committees-current.yaml")
     memberships = YAML.load open("data/unitedstates/congress-legislators/committee-membership-current.yaml")
     
+    # committees who do not spell out what congress they are in
+    bad_committees = []
+    # *current* committees for which we lack members
+    missing_members = []
+
     legislator_cache = {}
 
     count = 0
     sub_count = 0
+
     
     # we only store current committees
     Committee.delete_all
@@ -34,15 +43,32 @@ class Committees
     # store committees and subcommittees as peers, *and* nest subcommittees inside their committee
     current_committees.each do |us_committee|
       committee_id = us_committee['thomas_id']
+      
+      # we need to know whether it's current or historical
+      unless us_committee['congresses']
+        bad_committees << committee_id
+        next
+      end
+
       committee = Committee.find_or_initialize_by committee_id: committee_id
       
       committee.attributes = attributes_for us_committee
-      committee.attributes = memberships_for committee_id, memberships, legislator_cache
+
+      if committee[:congresses].include?(session)
+        committee.attributes = memberships_for committee, memberships, legislator_cache, missing_members
+      end
 
       subcommittees = []
       (us_committee['subcommittees'] || []).each do |us_subcommittee|
         subcommittee_id = us_subcommittee['thomas_id']
         full_id = [committee_id, subcommittee_id].join ""
+
+        # we need to know whether it's current or historical
+        unless us_subcommittee['congresses']
+          puts "No 'congresses' field for #{full_id}" if options[:debug]
+          bad_committees << full_id
+          next
+        end
 
         subcommittee = Committee.find_or_initialize_by committee_id: full_id
 
@@ -51,9 +77,11 @@ class Committees
         subcommittees << attributes
         subcommittee.attributes = attributes
 
-        subcommittee[:parent_committee_id] = committee_id
-        subcommittee.attributes = memberships_for full_id, memberships, legislator_cache
+        if committee[:congresses].include?(session)
+          subcommittee.attributes = memberships_for committee, memberships, legislator_cache, missing_members
+        end
 
+        subcommittee[:parent_committee_id] = committee_id
         subcommittee.save!
         sub_count += 1
       end
@@ -62,6 +90,14 @@ class Committees
       
       committee.save!
       count += 1
+    end
+
+    if bad_committees.any?
+      Report.warning self, "Unable to process #{bad_committees.size} committees", bad_committees: bad_committees
+    end
+
+    if missing_members.any?
+      Report.warning self, "Missing members for #{missing_members.size} current committees", missing_members: missing_members
     end
     
     Report.success self, "Processed #{count} committees and #{sub_count} subcommittees"
@@ -83,6 +119,10 @@ class Committees
       attributes[:office] = us_committee['address'].split("; ").first
     end
 
+    if us_committee['congresses']
+      attributes[:congresses] = us_committee['congresses'].split(/, ?/).map &:to_i
+    end
+
     if parent_committee
       attributes[:chamber] = parent_committee[:chamber]
       attributes[:subcommittee] = true
@@ -94,9 +134,13 @@ class Committees
     attributes
   end
 
-  def self.memberships_for(committee_id, memberships, legislator_cache)
+  # could be either a committee or subcommittee, this function should be blind
+  def self.memberships_for(committee, memberships, legislator_cache, missing_members)
+    committee_id = committee.committee_id
+
     unless memberships[committee_id]
-      puts "MISSING MEMBERSHIPS for #{committee_id}"
+      puts "MISSING MEMBERSHIP for #{committee_id}"
+      missing_members << committee_id
       return {}
     end
 
