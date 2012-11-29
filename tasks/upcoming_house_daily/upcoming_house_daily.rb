095be8608cf2dd93ab90c1244b05c1cae52d4766
Eric Mill <konklone@gmail.com>
2012-11-29 00:22:13 -0500

    Removed unneeded tasks

--- tasks/upcoming_house_daily/upcoming_house_daily.rb
+++ tasks/upcoming_house_daily/upcoming_house_daily.rb
@@ -17,7 +17,7 @@ class UpcomingHouseDaily
     
     doc = Nokogiri::HTML html
     
-    permalink, posted_at = Utils.permalink_and_date_from_house_gop_whip_notice(url, doc)
+    permalink, posted_at = permalink_and_date_from_house_gop_whip_notice(url, doc)
 
     unless posted_at
       Report.warning self, "Couldn't find date in House Republican daily schedule page, in either PDF or header, can't go on", :url => url
@@ -70,6 +70,46 @@ class UpcomingHouseDaily
     
     Report.success self, "Saved #{upcoming_count} upcoming bills for the House for #{legislative_day}"
     
+  end
+
+  # should work for both daily and weekly house notices
+  # http://majorityleader.gov/floor/daily.html
+  # http://majorityleader.gov/floor/weekly.html
+  #
+  # If the PDF link is valid, and has a date in it, then use that date, and use it as the permalink
+  # If not, extract the date from the header, and use the original url as the permalink
+  def self.permalink_and_date_from_house_gop_whip_notice(url, doc)
+    date = nil
+    permalink = url # default to the HTML page, unless we can get a valid PDF out of this
+
+    # first preference is to get the date from the PDF URL
+    links = (doc / :a).select {|x| x.text =~ /printable pdf/i}
+    a = links.first
+
+    date_results = nil
+
+    if a
+      pdf_url = a['href']
+      date_results = pdf_url.scan(/\/([^\/]+)\.pdf$/i)
+    end
+    
+    if date_results and date_results.any? and date_results.first.any?
+      date_str = date_results.first.first
+      month, day, year = date_str.split "-"
+      date = Utils.noon_utc_for Time.local(year, month, day)
+      permalink = pdf_url
+
+    # but if the PDF URL is messed up, try to get it from the header
+    else
+      begin
+        date = Date.parse doc.css("#news_text").first.css("b").first.text
+        date = Utils.noon_utc_for date.to_time
+      rescue ArgumentError
+        date = nil
+      end
+    end
+
+    [permalink, date]
   end
   
 end
--- tasks/utils.rb
+++ tasks/utils.rb
@@ -588,43 +588,5 @@ module Utils
     new_ids
   end
 
-  # should work for both daily and weekly house notices
-  # http://majorityleader.gov/floor/daily.html
-  # http://majorityleader.gov/floor/weekly.html
-  #
-  # If the PDF link is valid, and has a date in it, then use that date, and use it as the permalink
-  # If not, extract the date from the header, and use the original url as the permalink
-  def self.permalink_and_date_from_house_gop_whip_notice(url, doc)
-    date = nil
-    permalink = url # default to the HTML page, unless we can get a valid PDF out of this
-
-    # first preference is to get the date from the PDF URL
-    links = (doc / :a).select {|x| x.text =~ /printable pdf/i}
-    a = links.first
-
-    date_results = nil
-
-    if a
-      pdf_url = a['href']
-      date_results = pdf_url.scan(/\/([^\/]+)\.pdf$/i)
-    end
-    
-    if date_results and date_results.any? and date_results.first.any?
-      date_str = date_results.first.first
-      month, day, year = date_str.split "-"
-      date = noon_utc_for Time.local(year, month, day)
-      permalink = pdf_url
-
-    # but if the PDF URL is messed up, try to get it from the header
-    else
-      begin
-        date = Date.parse doc.css("#news_text").first.css("b").first.text
-        date = noon_utc_for date.to_time
-      rescue ArgumentError
-        date = nil
-      end
-    end
-
-    [permalink, date]
-  end
+  
 end
