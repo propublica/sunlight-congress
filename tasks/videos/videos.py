384f8f244fb8910dfe6b7616fb6a0972b5d1c7b6
Eric Mill <konklone@gmail.com>
2012-12-04 14:55:06 -0500

    Fixed a roll_ids array bug

--- tasks/videos/videos.py
+++ tasks/videos/videos.py
@@ -186,7 +186,7 @@ def get_clips_for_senate(db, clip_id, congress, duration, year):
             c['roll_ids'] = r
             for ro in r:
                 if ro not in roll_ids:
-                    roll_ids.append(r)
+                    roll_ids.append(ro)
         
         if b: 
             c['bill_ids'] = b
@@ -255,7 +255,7 @@ def get_markers(db, client_name, clip_id, congress, chamber):
                 c['roll_ids'] = r
                 for ro in r:
                     if ro not in roll_ids:
-                        roll_ids.append(r)
+                        roll_ids.append(ro)
             
             if b: 
                 c['bill_ids'] = b
