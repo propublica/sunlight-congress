e3a59570f2bf49dc694613d33403b68c3723c141
Eric Mill <konklone@gmail.com>
2012-12-21 13:53:11 -0500

    Moved docs out

--- docs/post.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p output
-
-curl -X POST --data-urlencode content@index.md --data-urlencode name="Congress API" --data-urlencode twitter=sunlightlabs "http://documentup.com/compiled" > output/index.html
