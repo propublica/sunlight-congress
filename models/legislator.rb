410f0b12f339aca59a315aee7d01a803131733e1
Eric Mill <konklone@gmail.com>
2013-01-16 10:24:03 -0500

    Add fax and birthday fields to default legislator fields

--- models/legislator.rb
+++ models/legislator.rb
@@ -5,9 +5,9 @@ class Legislator
   basic_fields  :bioguide_id, :thomas_id, :lis_id, :fec_ids,
       :votesmart_id, :crp_id, :govtrack_id,
       :title, :first_name, :nickname, :middle_name, :last_name, :name_suffix, 
-      :other_names, :gender, :in_office, :senate_class,
+      :other_names, :gender, :in_office, :senate_class, :birthday,
       :state, :state_name, :party, :chamber, :district,
-      :phone, :office, :website, :contact_form,
+      :phone, :fax, :office, :website, :contact_form,
       :twitter_id, :facebook_id, :youtube_id
 
   search_fields :first_name, :last_name, :middle_name, :nickname, "other_names.last"
