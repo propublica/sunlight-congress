d647545873332f2895c6ab26c0445a6b845e500f
Eric Mill <konklone@gmail.com>
2013-05-28 15:14:09 -0400

    Removing mention of amendments from vote matching for the time being - yeesh

--- tasks/votes_house/votes_house.rb
+++ tasks/votes_house/votes_house.rb
@@ -3,17 +3,17 @@ require 'nokogiri'
 class VotesHouse
 
   # Syncs vote data with the House of Representatives.
-  # 
-  # By default, looks through the Clerk's EVS pages, and 
+  #
+  # By default, looks through the Clerk's EVS pages, and
   # re/downloads data for the last 10 roll call votes.
-  # 
-  # Options can be passed in to archive whole years, which can ignore 
+  #
+  # Options can be passed in to archive whole years, which can ignore
   # already downloaded files (to support resuming).
-  # 
+  #
   # options:
   #   force: if archiving, force it to re-download existing files.
   #   year: archive an entire year of data. 'current' for current year (defaults to latest 20)
-  #   number: only download a specific roll call vote number for the given year. Ignores other options, except for year. 
+  #   number: only download a specific roll call vote number for the given year. Ignores other options, except for year.
   #   limit: only download a certain number of votes (stop short, useful for testing/development)
   #   skip_text: don't search index related text
 
@@ -36,7 +36,7 @@ class VotesHouse
         Report.failure self, "Failed to find the latest new roll on the clerk's page, can't go on."
         return
       end
-      
+
       if options[:year] # year implies archive
         from_roll = 1
       else
@@ -46,7 +46,7 @@ class VotesHouse
       end
 
       to_get = (from_roll..latest_roll).to_a.reverse
-      
+
       if options[:limit]
         to_get = to_get.first options[:limit].to_i
       end
@@ -57,7 +57,7 @@ class VotesHouse
     download_failures = []
     missing_bioguide_ids = []
     missing_bill_ids = []
-    missing_amendment_ids = []
+    # missing_amendment_ids = []
 
     legislators = {}
     Legislator.only(Legislator.basic_fields).all.each do |legislator|
@@ -76,20 +76,20 @@ class VotesHouse
       doc = Nokogiri::XML open(destination_for(year, number))
 
       puts "[#{roll_id}] Saving vote information..." if options[:debug]
-      
+
       # 404s for missing votes return an HTML doc that won't make sense
       # example of this: there was no roll 484 in 2011 for a while, due to procedural foulup
-      unless doc.at(:congress) 
+      unless doc.at(:congress)
         puts "\tNot a valid XML vote, skipping" if options[:debug]
         next
       end
-      
+
       congress = doc.at(:congress).inner_text.to_i
-      
+
       bill_type, bill_number = bill_code_for doc
       bill_id = (bill_type and bill_number) ? "#{bill_type}#{bill_number}-#{congress}" : nil
-      amendment_id = amendment_id_for doc, bill_id
-      
+      # amendment_id = amendment_id_for doc, bill_id
+
       voter_ids, voters = votes_for doc, legislators, missing_bioguide_ids
       roll_type = doc.at("vote-question").inner_text
       question = question_for doc, roll_type, bill_type, bill_number
@@ -98,7 +98,7 @@ class VotesHouse
         puts "[#{roll_id}] VACATED vote detected"
         next
       end
-      
+
       vote = Vote.find_or_initialize_by roll_id: roll_id
       vote.attributes = {
         vote_type: Utils.vote_type_for(roll_type, roll_type),
@@ -107,12 +107,12 @@ class VotesHouse
         number: number,
 
         congress: congress,
-        
+
         roll_type: roll_type,
         question: question,
         result: doc.at("vote-result").inner_text,
         required: required_for(doc),
-        
+
         voted_at: voted_at_for(doc),
         voter_ids: voter_ids,
         voters: voters,
@@ -122,7 +122,7 @@ class VotesHouse
         source: url_for(year, number),
         url: url_for(year, number)
       }
-      
+
       if bill_id
         vote[:bill_id] = bill_id
         if bill = Utils.bill_for(bill_id)
@@ -131,7 +131,7 @@ class VotesHouse
           missing_bill_ids << {roll_id: roll_id, bill_id: bill_id}
         end
       end
-      
+
       # if amendment_id
       #   if amendment = Amendment.where(:amendment_id => amendment_id).only(Amendment.basic_fields).first
       #     vote.attributes = {
@@ -142,7 +142,7 @@ class VotesHouse
       #     missing_amendment_ids << {:roll_id => roll_id, :amendment_id => amendment_id}
       #   end
       # end
-      
+
       vote.save!
 
       count += 1
@@ -156,14 +156,14 @@ class VotesHouse
       missing_bioguide_ids = missing_bioguide_ids.uniq
       Report.warning self, "Found #{missing_bioguide_ids.size} missing Bioguide IDs. Vote counts on roll calls may be inaccurate until these are fixed.", {missing_bioguide_ids: missing_bioguide_ids}
     end
-    
+
     if missing_bill_ids.any?
       Report.note self, "Found #{missing_bill_ids.size} missing bill_id's while processing votes.", {missing_bill_ids: missing_bill_ids}
     end
-    
-    if missing_amendment_ids.any?
-      Report.warning self, "Found #{missing_amendment_ids.size} missing amendment_id's while processing votes.", {missing_amendment_ids: missing_amendment_ids}
-    end
+
+    # if missing_amendment_ids.any?
+    #   Report.warning self, "Found #{missing_amendment_ids.size} missing amendment_id's while processing votes.", {missing_amendment_ids: missing_amendment_ids}
+    # end
 
     Report.success self, "Successfully synced #{count} House roll call votes for #{year}"
   end
@@ -207,7 +207,7 @@ class VotesHouse
       failures << {message: "Couldn't download", url: url, destination: destination}
       return false
     end
-      
+
     # 404s come back as 200's, and are HTML documents
     unless curl.content_type == "text/xml"
       failures << {message: "Probably 404", url: url, destination: destination, content_type: curl.content_type}
@@ -215,13 +215,13 @@ class VotesHouse
       return false
     end
 
-    # sanity check on files less than expected - 
+    # sanity check on files less than expected -
     # most are ~82K, so if something is less than 80K, check the XML for malformed errors
     if curl.downloaded_content_length < 80000
       # retry once, quick check
       puts "\tRe-downloading once, looked truncated" if options[:debug]
       curl = Utils.curl(url, destination)
-      
+
       if curl.downloaded_content_length < 80000
         begin
           Nokogiri::XML(open(destination)) {|config| config.strict}
@@ -242,10 +242,10 @@ class VotesHouse
   # latest roll number on the House Clerk's listing of latest votes
   def self.latest_roll_for(year, options = {})
     url = "http://clerk.house.gov/evs/#{year}/index.asp"
-    
+
     puts "[#{year}] Fetching index page for year from House Clerk..." if options[:debug]
     return nil unless doc = Utils.html_for(url)
-    
+
     element = doc.css("tr td a").first
     return nil unless element and element.text.present?
     number = element.text.to_i
@@ -263,7 +263,7 @@ class VotesHouse
       "1/2"
     end
   end
-  
+
   def self.vote_mapping
     {
       "Aye" => "Yea",
@@ -274,13 +274,13 @@ class VotesHouse
   def self.votes_for(doc, legislators, missing_ids)
     voter_ids = {}
     voters = {}
-    
+
     doc.search("//vote-data/recorded-vote").each do |elem|
       vote = (elem / 'vote').text
       vote = vote_mapping[vote] || vote # check to see if it should be standardized
-      
+
       bioguide_id = (elem / 'legislator').first['name-id']
-      
+
       if legislators[bioguide_id]
         voter = legislators[bioguide_id]
         bioguide_id = voter['bioguide_id']
@@ -291,16 +291,16 @@ class VotesHouse
         missing_ids << [bioguide_id, number]
       end
     end
-    
+
     [voter_ids, voters]
   end
-  
+
   def self.create_bill(bill_id, doc)
     bill = Utils.bill_from bill_id
     bill.save!
     bill
   end
-  
+
   # returns bill type and number
   def self.bill_code_for(doc)
     elem = doc.at 'legis-num'
@@ -308,7 +308,7 @@ class VotesHouse
       code = elem.text.strip.gsub(' ', '').downcase
       type = code.gsub /\d/, ''
       number = code.gsub type, ''
-      
+
       if !["hr", "hres", "hjres", "hconres", "s", "sres", "sjres", "sconres"].include?(type)
         return nil, nil
       else
@@ -318,15 +318,15 @@ class VotesHouse
       nil
     end
   end
-  
-  def self.amendment_id_for(doc, bill_id)
-    if bill_id and (elem = doc.at("amendment-num"))
-      if result = Amendment.where(:bill_id => bill_id, :bill_sequence => elem.text.to_i).only(:amendment_id).first
-        result['amendment_id']
-      end
-    end
-  end
-  
+
+  # def self.amendment_id_for(doc, bill_id)
+  #   if bill_id and (elem = doc.at("amendment-num"))
+  #     if result = Amendment.where(:bill_id => bill_id, :bill_sequence => elem.text.to_i).only(:amendment_id).first
+  #       result['amendment_id']
+  #     end
+  #   end
+  # end
+
   def self.zero_prefix(number)
     if number < 10
       "00#{number}"
@@ -336,7 +336,7 @@ class VotesHouse
       number
     end
   end
-  
+
   def self.voted_at_for(doc)
     datestamp = doc.at("action-date").inner_text
     timestamp = doc.at("action-time").inner_text
@@ -346,11 +346,11 @@ class VotesHouse
   def self.question_for(doc, roll_type, bill_type, bill_number)
     question = roll_type.dup
     desc = doc.at("vote-desc").inner_text
-    
+
     if bill_type and bill_number
       question << " -- " + Utils.format_bill_code(bill_type, bill_number)
     end
-    
+
     if desc.present?
       question << " -- " + desc
     end
