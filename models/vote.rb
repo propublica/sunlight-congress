0f4d250f5d20f9aab19e437e2439e6af31667420
Eric Mill <konklone@gmail.com>
2013-06-17 19:24:32 -0400

    Add a couple indexes

--- models/vote.rb
+++ models/vote.rb
@@ -1,30 +1,32 @@
 class Vote
   include Api::Model
   publicly :queryable
-  
+
   search_fields :question
 
-  basic_fields :roll_id, :number, :year, :chamber, :congress, 
+  basic_fields :roll_id, :number, :year, :chamber, :congress,
     :question, :result, :voted_at, :required,
-    :roll_type, :vote_type, 
+    :roll_type, :vote_type,
     :bill_id, :amendment_id, :source, :url
-  
+
 
   # MongoDB behavior
 
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   index roll_id: 1
   index chamber: 1
   index year: 1
   index congress: 1
-  
+
   index bill_id: 1
   index amendment_id: 1
 
   index result: 1
   index voted_at: 1
+  index({voted_at: 1, roll_id: 1})
+  index({voted_at: 1, roll_id: -1})
 
   index roll_type: 1
   index vote_type: 1
