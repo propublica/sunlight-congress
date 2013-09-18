9141628fd45be85fc3bab8fec356e5fe709767f3
Eric Mill <eric@konklone.com>
2013-09-18 17:16:22 -0400

    Fixed up senate floor scraper for the revamped Periodical Press Gallery

--- tasks/floor_senate/floor_senate.rb
+++ tasks/floor_senate/floor_senate.rb
@@ -3,87 +3,90 @@
 require 'nokogiri'
 
 class FloorSenate
-  
+
   # options:
+  #   range: how many days before (or after) today to parse updates for.
+  #          defaults to 1.
   #   no_sleep: don't sleep for 1s between updates - produces inaccurate data,
   #             but is tolerable to run in dev
   def self.run(options = {})
-    
+    range = options[:range] ? options[:range].to_i : 1
+
     count = 0
     failures = []
-    
+
     html = nil
     begin
-      html = Utils.curl "http://www.senate.gov/galleries/pdcl/?break_cache=#{Time.now.to_i}"
+      html = Utils.curl "http://www.periodicalpress.senate.gov/?break_cache=#{Time.now.to_i}"
     rescue Timeout::Error, Errno::ECONNRESET, Errno::ETIMEDOUT, Errno::ENETUNREACH
       Report.warning self, "Network error on fetching the floor log, can't go on."
       return
     end
-    
+
     doc = Nokogiri::HTML html
-    
-    unless title_elem = title_elem_for(doc)
+
+    unless container = doc.css("div.entry-content").first
       Report.warning self, "Can't locate title of the floor log, can't go on."
       return
     end
-    
+
     # accumulate results in hash, keyed by date string, values are array of text updates
     updates = {}
     current_date = nil
-    
+
     warnings = []
 
-    (title_elem.parent / :p).each do |item|
+    (container.parent / :p).each do |item|
       # ignore headers and footer
       next if ["senate floor proceedings", "today's senate floor log", "\302\240"].include?(item.text.strip.downcase)
-      next if [/ShockwaveFlash/i, /for reference only/i].find {|r| item.text.strip =~ r}
+      next if [/archived floor logs/i, /floor lof is for reference only/i].find {|r| item.text.strip =~ r}
 
-      if item['align'] == 'center'
+      if item['style'] =~ /text-align: center/i
         if Time.zone.parse(item.text)
           current_date = Utils.utc_parse(item.text).strftime "%Y-%m-%d"
           updates[current_date] ||= []
         else
           puts "Skipping center-aligned p with text #{item.text}" if options[:debug]
         end
-      
+
       else # item['align'] == 'left' or item['align'].nil?
         if current_date.nil?
-          Report.warning self, "Unexpected HTML, got to a update without a date, skipping"
+          warnings << {msg: "Unexpected HTML, got to a update without a date, skipping", text: item.text}
           next
         end
-        
+
         updates[current_date] << clean_text(item.text)
       end
     end
-    
+
     # We'll run this every 5 minutes, so we'll assign a timestamp to an item as soon we find it, if it doesn't exist already
     # If it does exist...we leave it alone.
     # This is *not* an archival script, and the timestamps will also be inaccurate at first - we must accept this.
-    
+
     congress = Utils.current_congress
 
     today = Time.now.midnight
-    
+
     updates.keys.sort.each do |legislative_day|
       # skip unless it's within a day of today
       this = Time.parse(legislative_day).midnight
-      if (this > (today + 1.day)) or (this < (today - 1.day))
+      if (this > (today + range.days)) or (this < (today - range.days))
         next
       end
-      
+
       todays = FloorUpdate.where(legislative_day: legislative_day).all.map {|u| u['update']}
       items = updates[legislative_day]
-      
+
       # puts legislative_day
-      
+
       items.each do |item|
-        
+
         # leave existing items alone
         if todays.include?(item)
           puts "Found a dupe, ignoring" if options[:debug]
           next
         end
-        
+
         floor_update = FloorUpdate.new(
           chamber: "senate",
           congress: congress,
@@ -94,11 +97,11 @@ class FloorSenate
           roll_ids: extract_rolls(item),
           legislator_ids: extract_legislators(item)
         )
-        
+
         if floor_update.save
           count += 1
           puts "[#{floor_update.timestamp.strftime("%Y-%m-%d %H:%M:%S")}] New floor update on leg. day #{legislative_day}" if options[:debug]
-          
+
           # sleep for a second so that if we discover multiple things at once on the same day it doesn't get the same timestamp
           sleep 1 unless options[:no_sleep]
         else
@@ -107,7 +110,7 @@ class FloorSenate
         end
       end
     end
-    
+
     if failures.any?
       Report.failure self, "Failed to save #{failures.size} floor updates.", failures: failures
     end
@@ -115,29 +118,24 @@ class FloorSenate
     if warnings.any?
       Report.warning self, "Warnings while scanning floor", warnings: warnings
     end
-    
+
     Report.success self, "Saved #{count} new floor updates"
   end
-  
-  def self.title_elem_for(doc)
-    # either one is fine, I just want the parent to do the right scoping
-    (doc / :p).find {|e| (e.text.strip =~ /SENATE\s+FLOOR\s+PROCEEDINGS/i) || (e.text.strip =~ /TODAY'S\s+SENATE\s+FLOOR\s+LOG/i)}
-  end
-  
+
   def self.extract_bills(text)
     congress = Utils.current_congress
     matches = text.scan(/((S\.|H\.)(\s?J\.|\s?R\.|\s?Con\.| ?)(\s?Res\.?)*\s?\d+)/i).map {|r| r.first}.uniq.compact
     matches.map {|code| "#{code.tr(" ", "").tr('.', '').downcase}-#{congress}" }
   end
-  
+
   def self.extract_rolls(text)
     [] # unsure how to do this, they never use the roll number that I can see!
   end
-  
+
   def self.extract_legislators(text)
     []
   end
-  
+
   def self.clean_text(text)
     text.
       gsub("\342\200\231", "'").
@@ -148,5 +146,5 @@ class FloorSenate
       gsub("\n", "\n\n").
       strip
   end
-  
+
 end
