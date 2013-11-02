c0cd5afb546fc0deab4f8520c4a7ba9b99b43b68
Eric Mill <eric@konklone.com>
2013-11-02 00:53:52 -0400

    old comments

--- config/cron/scripts/us-sync-igs.sh
+++ config/cron/scripts/us-sync-igs.sh
@@ -3,5 +3,5 @@
 cd $HOME/unitedstates/inspectors-general
 source $HOME/.virtualenvs/inspectors/bin/activate
 
-# get all bills in the current session, re-download everything
+# get latest IG reports
 ./inspectors/usps.py > $HOME/congress/shared/cron/output/us-sync-igs.txt 2>&1
