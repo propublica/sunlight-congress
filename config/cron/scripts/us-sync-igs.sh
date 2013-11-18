d48bc3316f76c65b23833ddc88cf03b3c8e178f7
Eric Mill <eric@konklone.com>
2013-11-18 16:37:20 -0500

    Get dhs too

--- config/cron/scripts/us-sync-igs.sh
+++ config/cron/scripts/us-sync-igs.sh
@@ -4,4 +4,4 @@ cd $HOME/unitedstates/inspectors-general
 source $HOME/.virtualenvs/inspectors/bin/activate
 
 # get latest IG reports
-./inspectors/usps.py > $HOME/congress/shared/cron/output/us-sync-igs.txt 2>&1
+./inspectors/usps.py && ./inspectors/dhs.py > $HOME/congress/shared/cron/output/us-sync-igs.txt 2>&1
