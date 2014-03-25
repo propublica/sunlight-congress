badf7be8c6f8c88fa5ae83118dd6dc9747dce4e2
Eric Mill <eric@konklone.com>
2014-03-25 19:56:30 -0400

    I am the best, wow

--- api/searchable.rb
+++ api/searchable.rb
@@ -146,7 +146,6 @@ module Searchable
 
     begin
       results = @search_client.search request
-      raise Exception.new "oh no!"
     rescue Exception => exc
       return {
         error: exception_to_hash(exc)
@@ -173,7 +172,6 @@ module Searchable
       start = Time.now
       results = @explain_client.search request
       elapsed = Time.now - start
-      raise Exception.new "oh no!"
     rescue Exception => exc
       return {
         error: exception_to_hash(exc),
