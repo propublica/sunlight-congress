9afd2649df7771934f6c669630e07a0921b463b3
Eric Mill <eric@konklone.com>
2014-04-07 17:24:21 -0400

    should still be id

--- models/clip.rb
+++ models/clip.rb
@@ -2,10 +2,10 @@ class Clip
   include Api::Model
   publicly :searchable
 
-  basic_fields :video_id, :clip_id, :video_clip_id, 
+  basic_fields :video_id, :id, :video_clip_id,
     :events, :published_at,
     :offset, :duration, :srt_link,
-    :legislator_names, 
+    :legislator_names,
     :roll_ids, :bill_ids, :legislator_ids
 
   search_fields :events, :captions, :legislator_names
