ce8f5e418c621ede3a04ab3bdd280bbcae5f5663
Eric Mill <eric@konklone.com>
2014-04-08 17:27:22 -0400

    adding some better instrumentation for key replication, removing outdated key replication methods

--- api/sunlight.rb
+++ api/sunlight.rb
@@ -2,7 +2,7 @@
 
 before do
   if request.post? and !SunlightServices.verify(params, Environment.config[:services][:shared_secret], Environment.config[:services][:api_name])
-    halt 403, 'Bad signature' 
+    halt 403, 'Bad signature'
   end
 end
 
@@ -11,36 +11,14 @@ post '/analytics/replicate_key/:key/' do
     halt 403, "Missing a key, email, and/or status: #{params.inspect}"
   end
 
-  key = ApiKey.find_or_create_by key: params[:key]
-  key.email = params[:email]
-  key.status = params[:status]
-  key.save!
-end
+  puts "Replicating key for: #{params.inspect}"
 
-post '/analytics/create_key/' do
-  unless ApiKey.new(key: params[:key], email: params[:email], status: params[:status]).save
-    halt 403, "Could not create key, duplicate key or email"
-  end
-end
-
-post '/analytics/update_key/' do
-  unless key = ApiKey.where(key: params[:key]).first
-    halt 404, 'Could not locate API key by the given key'
-  end
-  
-  key.attributes = {email: params[:email], status: params[:status]}
-  unless key.save
-    halt 403, "Could not update key, errors: #{key.errors.full_messages.join ', '}"
-  end
-end
-
-post '/analytics/update_key_by_email/' do
-  unless key = ApiKey.where(email: params[:email]).first
-    halt 404, 'Could not locate API key by the given email'
-  end
-
-  key.attributes = {key: params[:key], status: params[:status]}
-  unless key.save
-    halt 403, "Could not update key, errors: #{key.errors.full_messages.join ', '}"
+  begin
+    key = ApiKey.find_or_create_by key: params[:key]
+    key.email = params[:email]
+    key.status = params[:status]
+    key.save!
+  rescue Exception => ex
+    Email.report Report.exception("SunlightServices", "Error replicating key.", ex, {key: key.attributes.dup})
   end
 end
