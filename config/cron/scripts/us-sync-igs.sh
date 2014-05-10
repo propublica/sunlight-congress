4ddc94df54e833545e4a98ea9ce8a94028cb340b
Eric Mill <eric@konklone.com>
2014-05-09 23:26:51 -0400

    new syntax for using IG scrapers

--- config/cron/scripts/us-sync-igs.sh
+++ config/cron/scripts/us-sync-igs.sh
@@ -3,5 +3,5 @@
 cd $HOME/unitedstates/inspectors-general
 source $HOME/.virtualenvs/inspectors/bin/activate
 
-# get latest IG reports
-./inspectors/usps.py && ./inspectors/dhs.py > $HOME/congress/shared/cron/output/us-sync-igs.txt 2>&1
+# get latest IG reports from all 'safe' scrapers
+./igs --safe > $HOME/congress/shared/cron/output/us-sync-igs.txt 2>&1
