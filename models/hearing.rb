58c2ebcce7596247c55af34f9fdaa20c8a69819a
Eric Mill <eric@konklone.com>
2014-05-14 16:15:30 -0400

    Adding an index for new house_hearing_id field

--- models/hearing.rb
+++ models/hearing.rb
@@ -1,22 +1,23 @@
 class Hearing
   include Api::Model
   publicly :queryable
-  
+
   basic_fields :committee_id, :subcommittee_id,
     :congress, :chamber, :occurs_at, :dc,
-    :room, :description, :url, 
-    :bill_ids, 
+    :room, :description, :url,
+    :bill_ids,
     :hearing_type, :house_hearing_id # house only for now
-  
+
   search_fields :description
 
-  
-  
+
+
   include Mongoid::Document
   include Mongoid::Timestamps
-  
+
   index chamber: 1
   index committee_id: 1
+  index house_hearing_id: 1
   index subcommittee_id: 1
   index occurs_at: 1
   index congress: 1
