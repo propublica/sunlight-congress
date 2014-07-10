43a669dd50cc61b71cd5bf8577d0eeee71f83c6c
Lindsay Young <lyoung@sunlightfoundation.com>
2014-07-10 10:21:30 -0400

    adding oc email to bulk download, as requested by @angai369 already in legislators endpoint, though I would like to clean that up a bit. fix #461

--- tasks/legislators_bulk/legislators_bulk.rb
+++ tasks/legislators_bulk/legislators_bulk.rb
@@ -38,7 +38,7 @@ class LegislatorsBulk
       csv << %w{
         title firstname middlename lastname name_suffix nickname
         party state district in_office gender
-        phone fax website webform congress_office
+        phone fax website oc_email webform congress_office
         bioguide_id votesmart_id fec_id govtrack_id crp_id twitter_id
         congresspedia_url youtube_url facebook_id
         official_rss senate_class birthdate
