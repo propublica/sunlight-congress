cdd243bd2a2fb4873d641c13c860f1ac031e0649
Lindsay Young <lyoung@sunlightfoundation.com>
2014-08-01 13:30:05 -0400

    new majority leader website

--- tasks/upcoming_house/upcoming_house.rb
+++ tasks/upcoming_house/upcoming_house.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 
 class UpcomingHouse
-
+# not anymore
   # options:
   #
   #   range: "day" or "week" (default)
@@ -14,12 +14,12 @@ class UpcomingHouse
 
     page = {day: "daily", week: "weekly"}[range.to_sym]
     source_type = "house_#{page}"
-    url = "http://majorityleader.gov/floor/#{page}.html"
+    url = "http://www.majorityleader.gov/floor/"
 
     begin
       html = open(url).read
     rescue Timeout::Error, Errno::ECONNRESET, Errno::ETIMEDOUT, Errno::ENETUNREACH
-      Report.warning self, "Network error on fetching House Republican daily schedule, can't go on.", :url => url
+      Report.warning self, "Network error on fetching House Republican daily schedule at- #{url}, can't go on.", :url => url
       return
     end
 
@@ -29,7 +29,7 @@ class UpcomingHouse
 
     # we need a legislative day to anchor this around
     unless legislative_day
-      Report.warning self, "Couldn't find date in House Republican daily schedule page, in either PDF or header, can't go on", url: url
+      Report.warning self, "Couldn't find date in House Republican daily schedule page, #{url}, can't go on", url: url
       return
     end
 
@@ -48,15 +48,19 @@ class UpcomingHouse
     year = legislative_day.split("-").first
     congress = Utils.congress_for_year year
 
-    # grab all bill IDs mentioned in a giant blob of text
-    element = doc.css("div#news_text").first
+    # grab all bill IDs 
+    if page == 'weekly'
+      element = doc.css("div#weekly").first
+    else
+      element = doc.css("div#daily").first
+    end
+
     unless element
       Report.failure self, "Couldn't find the House majority leader daily schedule content node, can't go on"
       return
     end
 
     bill_ids = Utils.bill_ids_for element.text, congress
-
 
     # clear out associations at /bill
     Utils.flush_bill_upcoming! source_type
@@ -129,51 +133,55 @@ class UpcomingHouse
   end
 
   # should work for both daily and weekly house notices
-  # http://majorityleader.gov/floor/daily.html
-  # http://majorityleader.gov/floor/weekly.html
+  # http://majorityleader.gov/floor/#daily.html
+  # http://majorityleader.gov/floor/#weekly.html
   #
   # If the PDF link is valid, and has a date in it, then use that date, and use it as the permalink
   # If not, extract the date from the header, and use the original url as the permalink
   def self.details_from(url, doc)
-    date = nil
+    # Do I still need this?
     source_url = nil
 
-    # first preference is to get the date from the PDF URL
-    links = (doc / :a).select {|x| x.text =~ /printable pdf/i}
-    a = links.first
-
-    date_results = nil
-
-    if a
-      pdf_url = a['href']
-      pdf_url
-      date_results = pdf_url.gsub(/(DAILY|WEEKLY)%20/, '').gsub(/%20[^\.]*\.pdf/i, '.pdf').scan(/\/([^\/]+)\.pdf$/i)
-    end
-
-    if date_results and date_results.any? and date_results.first.any?
-      date_text = date_results.first.first
-      source_url = pdf_url
-
-      month, day, year = date_text.split "-"
-      year = ("20" + year) if year.size == 2
-      date_text = [year, month, day].join "-"
-
-    # but if the PDF URL is messed up, try to get it from the header
-    else
-      begin
-        date_text = doc.css("#news_text").first.css("b").first.text
-      rescue ArgumentError
-        date = nil
-      end
-    end
-
-    # sanity check, sometimes there are typos on the year
-    if (year.to_i != Time.zone.now.year) and (Utils.utc_parse(date_text) < 6.months.ago)
-      date_text.gsub! year, (year.to_i + 1).to_s
-    end
-
+    daily_section = doc.css('div#daily')
+    date_text = daily_section.css('h5.post-title')[0].text
     date = Utils.utc_parse(date_text).strftime "%Y-%m-%d"
 
+### none of this works anymore
+    # first preference is to get the date from the PDF URL
+    # links = (doc / :a).select {|x| x.text =~ /printable pdf/i}
+    # a = links.first
+    # if a
+    #   pdf_url = a['href']
+    #   pdf_url
+    #   date_results = pdf_url.gsub(/(DAILY|WEEKLY)%20/, '').gsub(/%20[^\.]*\.pdf/i, '.pdf').scan(/\/([^\/]+)\.pdf$/i)
+    # end
+
+    # if date_results and date_results.any? and date_results.first.any?
+    #   date_text = date_results.first.first
+    #   source_url = pdf_url
+
+    #   month, day, year = date_text.split "-"
+    #   year = ("20" + year) if year.size == 2
+    #   date_text = [year, month, day].join "-"
+
+    # # but if the PDF URL is messed up, try to get it from the header
+    # else
+    #   begin
+    #     date_text = doc.css("#news_text").first.css("b").first.text
+    #   rescue ArgumentError
+    #     date = nil
+    #   end
+    # end
+
+    month, day, year = date.split "-"
+    year = ("20" + year) if year.size == 2
+    date_text = [year, month, day].join "-"
+
+    # sanity check, sometimes there are typos on the year
+    if (year.to_i != Time.zone.now.year) and (Utils.utc_parse(date) < 6.months.ago)
+      date_text.gsub! year, (year.to_i + 1).to_s
+    end
+    # I don't think source_urls is going to be used
     [source_url, date]
   end
 
