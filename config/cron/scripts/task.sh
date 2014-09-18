8d44dea48e23d03bc048f86ee4a1dd5dd9a7e9e1
Sam Handler <sam.handler@gmail.com>
2014-09-18 17:15:42 -0400

    Remove lockfiles from some cron scripts
    The scripts using the lockfile command have not yet hit production, and we need different syntax for Ubuntu.
    
    This commit should not have side effects on our production servers, but I'm putting it in a pull request so people will be notified.

--- config/cron/scripts/task.sh
+++ config/cron/scripts/task.sh
@@ -5,10 +5,6 @@ source $HOME/.virtualenvs/congress/bin/activate
 cd $HOME/congress/current 
 
 FIRST=$1
-
-lockfile -r 10 $HOME/tmp/locks/$FIRST.lock
-
 shift
 rake task:$FIRST $@ > $HOME/congress/shared/cron/output/$FIRST.txt 2>&1
 
-rm -f $HOME/tmp/locks/$FIRST.lock
