e60bc69cff3183fddd028a86b2d2cc8436626a5f
Sam Handler <sam.handler@gmail.com>
2014-11-14 16:18:47 -0500

    Fix some environment issues in us-sync-upcoming-house-floor

--- config/cron/scripts/us-sync-upcoming-house-floor.sh
+++ config/cron/scripts/us-sync-upcoming-house-floor.sh
@@ -6,6 +6,9 @@ source $HOME/.virtualenvs/us-congress/bin/activate
 # get all upcomming bills on the House Floor for the week
 ./run upcoming_house_floor  > $HOME/congress/shared/log/cron/upcoming-house-floor.txt 2>&1
 
+# deactivate virtualenv from above
+deactivate 
+
 # now switch to congress 
 . $HOME/.bashrc 
 source $HOME/.virtualenvs/congress/bin/activate
