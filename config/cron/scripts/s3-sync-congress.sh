a29bff80c23d39d826def8dc2d6de65e3d8db1ce
Sam Handler <sam.handler@gmail.com>
2015-03-16 16:17:26 -0400

    Bump congress for s3 dump

--- config/cron/scripts/s3-sync-congress.sh
+++ config/cron/scripts/s3-sync-congress.sh
@@ -4,5 +4,4 @@ cd $HOME/unitedstates/us-sync
 source $HOME/.virtualenvs/us-sync/bin/activate
 
 # sync the current congress up to S3 each night
-./sync --congresses=113 > $HOME/congress/shared/log/cron/unitedstates-s3.txt 2>&1
-
+./sync --congresses=114 > $HOME/congress/shared/log/cron/unitedstates-s3.txt 2>&1
